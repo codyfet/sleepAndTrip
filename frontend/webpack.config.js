c2e607a8f3f98c57c4d60d5adae0d960a083c4f1
heyayes <stshakun@gmai.com>
2019-04-24 16:52:10 +0300

    добавлен лицевой конец

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
-var htmlPlugin = new HtmlWebpackPlugin({
-    template: "./dist/index.html",
-    filename: "./index.html"
-});
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: __dirname + '/dist/index.html',
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }, 
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.less$/,
-                use: ["style-loader", "css-loader", "less-loader"]
-            }
-        ],
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    resolve: {
-        //Automatically resolve certain extensions.
-        extensions: ['.less', '.js', '.jsx']
-    },
-    plugins: [htmlPlugin]
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            use: ["source-map-loader"],
+            enforce: "pre"
+        },
+        {
+            test: /\.(jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.(less|css)$/,
+            use: [
+              'style-loader',
+              { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
+              'less-loader',
+            ],
+          },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.less', '.css']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 };
